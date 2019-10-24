42a3bf2f6b194ac41e9be33772c2c8a7ae1285ea
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-10-24 18:21:44 -0300

    svelte-spa-router and home page

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,11 +1,19 @@
+
 <script>
-	export let name;
+
+  import Router, { location } from 'svelte-spa-router'
+  import routes from './router.js'
+
+  export let name
+  
 </script>
 
 <style>
 	h1 {
-		color: purple;
+		color: purple
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {$location}!</h1>
+
+<Router {routes}/>
