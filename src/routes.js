819fbcc265b8d6f48fa7735e3975258f59d22ce4
vinibispo <vini.bispo015@gmail.com>
2019-10-24 13:52:13 -0300

    ✨ (Client): Client basic
    Client basic

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const UserController = require('./Controller/UserController')
+const UserController = require('./controller/UserController')
+const ClientController = require('./controller/ClientController')
 
 router.get('/', (req, res)=>{
     res.send({hello: 'World'})
 })
 
-router.get('/users', UserController.index)
 
-router.post('/users',UserController.store)
-router.put('/users/:id',UserController.update)
-router.delete('/users/:id',UserController.remove)
+function User(){
+    router.get('/users', UserController.index)
+    router.post('/users',UserController.store)
+    router.put('/users/:id',UserController.update)
+    router.delete('/users/:id',UserController.remove)
+}
+User()
+function Clients(){
+    router.get('/users/:user_id/clients', ClientController.index)
+    router.post('/users/:user_id/clients', ClientController.store)
+    router.put('/users/:user_id/clients/:id', ClientController.update)
+    router.delete('/users/:user_id/clients/:id', ClientController.remove)
+}
+Clients()
 
 module.exports = router
