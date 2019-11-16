0a30f45af7dff41e87aaf68f36d388a87a8be058
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-11-16 10:24:36 -0300

    toast design

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -3,6 +3,7 @@
 
   import NavBar from './components/Navigation/NavBar.svelte'
   import Popup from './components/Popup/Popup.svelte'
+  import Toast from './components/Toast.svelte'
 
   import Router from 'svelte-spa-router'
 
@@ -15,6 +16,7 @@
 </style>
 
 <Popup/>
+<Toast/>
 
 <NavBar/>
 <Router {routes}/>
---
+++ front/src/components/Toast.svelte
@@ -0,0 +1,26 @@
+
+<script>
+
+
+
+</script>
+
+
+  <div class="Toast card rb shadow">
+    <span>açsldjfkasçljfdk</span>
+  </div>
+
+<style>
+
+.Toast {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translate(-50%,-50px);
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+}
+
+</style>
